179b6d77e40baabb7a9643f45846faca04f56273
Gael <gaelflores2907@gmail.com>
2026-01-30 14:16:25 -0700

    Se agrego el modelo de musica y la conexion a al API

---
+++ Api_Musical/Models/Cancion.cs
@@ -0,0 +1,16 @@
+﻿namespace Api_Musical.Models
+{
+    public class Cancion
+    {
+        public int id { get; set; }
+        public string titulo { get; set; }
+        public string preview { get; set; }
+        public int duracion { get; set; }
+
+        public String getTittle()
+        {
+            return titulo;
+        }
+
+    }
+}
