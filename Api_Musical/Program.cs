e912d0c5ffcb849ae55c74d5c9f9ca2c57f15fa5
Gael <gaelflores2907@gmail.com>
2026-02-02 14:16:40 -0700

    se termino todo lo relacionado con musica

--- Api_Musical/Program.cs
+++ Api_Musical/Program.cs
@@ -1,7 +1,11 @@
+using Api_Musical.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<DeezerService>();
 
 var app = builder.Build();
 
@@ -22,6 +26,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Deezer}/{action=Index}/{id?}");
 
 app.Run();
